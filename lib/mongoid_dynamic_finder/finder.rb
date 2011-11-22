2fee54a94c7442452586b5cb11d06848367f570b
Winus Peng <iwinux@gmail.com>
2011-11-22 10:14:34 +0800

    added support for 'bang' methods

--- lib/mongoid_dynamic_finder/finder.rb
+++ lib/mongoid_dynamic_finder/finder.rb
@@ -1,18 +1,30 @@
 module Mongoid
   module Finders
 
-   def method_missing(method_id, *arguments, &block)
+    def method_missing(method_id, *args, &block)
       conditions = {}
+      bang = false
+
       case method_id.to_s
-        when /^find_(all|last||first)_?by_([_a-zA-Z]\w*)$/
-          finder_type = !$1.blank? ? $1.to_sym : :all
-          $2.split(/_and_/).each_with_index do |attr, i|
-            conditions[attr] = arguments[i]
-          end
-          find(finder_type, :conditions => conditions)
+      when /^find_(all|last||first)_?by_([_a-zA-Z]\w*)(!?)$/
+        finder_type = $1.blank? ? :first : $1.to_sym
+        bang = true if $3 == '!'
+
+        $2.split(/_and_/).each_with_index do |attr, i|
+          conditions[attr] = args[i]
+        end
+
+        result = find(finder_type, :conditions => conditions)
+
+        if result.nil? and bang
+          raise Mongoid::Errors::DocumentNotFound.new(self.class, args)
         else
-          super
+          return result
+        end
+      else
+        super
       end
-    end 
+    end
+
   end
 end
